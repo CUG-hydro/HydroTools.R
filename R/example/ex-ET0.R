37e5c7d1de18768c6e26e08880aba7de226d2bd6
Dongdong Kong <kongdd@users.noreply.github.com>
2021-12-18 19:40:15 +0800

    fix `ET0_Penman48`

--- R/example/ex-ET0.R
+++ R/example/ex-ET0.R
@@ -1,3 +1,3 @@
 et0_pt = ET0_PT72(250, 25, D = 1)
-et0_penman54 = ET0_Penman54(250, 25, D = 1, wind = 2)
+et0_penman54 = ET0_Penman48(250, 25, D = 1, wind = 2)
 et0_fa098 = ET0_FAO98(250, 25, D = 1, wind = 2)
--- tests/testthat/test-ET0_models.R
+++ tests/testthat/test-ET0_models.R
@@ -2,7 +2,7 @@ test_that("ET0 models work", {
     Ra = cal_Rn(lat = 30, J = 1, ssd = 10, RH = 70, Tmin = 20, Tmax = 30, cld = 0.2)
 
     et0_pt72 = ET0_PT72(250, 25, D = 1)
-    et0_pm54 = ET0_Penman54(250, 25, D = 1, wind = 2)
+    et0_pm54 = ET0_Penman48(250, 25, D = 1, wind = 2)
     et0_fao98 = ET0_FAO98(250, 25, D = 1, wind = 2)
     
     expect_true(et0_pm54$ET0 > et0_pt72$ET0 / 1.26)
